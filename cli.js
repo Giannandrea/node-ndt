ad347011373172885b0a9857eae52b6c35122b68
mauro giannandrea <mgiannandrea@domotz.com>
2018-08-02 10:53:41 +0200

    add cli for speed_test

---
+++ cli.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+'use strict'
+
+var dns = require('dns');
+var meow = require('meow');
+var pkg = require('./package.json');
+var NodeNdt = require('./ndt');
+
+
+function p_exit(error) {
+    if (error)
+        console.error(error.message);
+    var status = error ? 0 : 1;
+    process.exit(status);
+    return;
+}
+
+// Check connection
+dns.lookup('mlab-ns.appspot.com', err => {
+    if (err && err.code === 'ENOTFOUND') {
+        p_exit(err);
+    }
+});
+
+var cli = meow(`
+  COMMANDS (only one)
+    --version: to see spdtst's current version.
+`, {
+    alias: {
+        version: pkg.version
+    }
+})
+
+var cb_alter = {
+    'onfinish': function (passed_results) {
+        console.log(passed_results);
+        p_exit();
+    }
+}
+
+try {
+    NodeNdt.run_test(cb_alter);
+} catch (error) {
+    p_exit(error.message);
+}
--- ndt.js
+++ ndt.js
@@ -1,144 +1,127 @@
 //var request = require('request');
-var http = require('http'); 
+var http = require('http');
 var url = require('url');
 WebSocket = require('ws');
 var os = require('os');
 NDTWrapper = require('./ndt_wrapper');
 
-function json_result_formatter(results, start_date, server) {
-    final_json = {}
-    final_json["annotations"] = {"platform": os.platform()};
-    final_json["data_format_version"] = "0.2.0";
-    final_json["measurement_start_time"] = start_date;
-    final_json["report_id"] = "1530626824934";
-    final_json["software_name"] = "domotz_speed_test";
-    final_json["software_version"] = "1.0.0";
-    final_json["test_keys"] = {
-        "advanced": {
-            "avg_rtt": results["avgrtt"],
-            "max_rtt": results["MaxRTT"],
-            "min_rtt": results["MinRTT"]
-        },
-        "server_infos": {
-            "failure": null,
-            "server_address": server
-        },
-        "simple": {
-            "download": results['s2cRate'] / 1000,
-            "fastest_test": "server_infos",
-            "ping": results["avgrtt"],
-            "upload": results['c2sRate'] / 1000
-        },
-        "test_c2s": [],
-        "test_s2c": []
+var NodeNdt = function NodeNdt() {
+    function json_result_formatter(results, start_date, server) {
+        final_json = {}
+        final_json["annotations"] = {
+            "platform": os.platform()
+        };
+        final_json["data_format_version"] = "0.2.0";
+        final_json["measurement_start_time"] = start_date;
+        final_json["report_id"] = "1530626824934";
+        final_json["software_name"] = "domotz_speed_test";
+        final_json["software_version"] = "1.0.0";
+        final_json["test_keys"] = {
+            "advanced": {
+                "avg_rtt": results["avgrtt"],
+                "max_rtt": results["MaxRTT"],
+                "min_rtt": results["MinRTT"]
+            },
+            "server_infos": {
+                "failure": null,
+                "server_address": server
+            },
+            "simple": {
+                "download": results['s2cRate'] / 1000,
+                "fastest_test": "server_infos",
+                "ping": results["avgrtt"],
+                "upload": results['c2sRate'] / 1000
+            },
+            "test_c2s": [],
+            "test_s2c": []
+        };
+        final_json["test_name"] = "ndt";
+        final_json["test_runtime"] = results["timesec"];
+        final_json["test_start_time"] = start_date;
+        final_json["test_version"] = "1.0";
+        return final_json;
     };
-    final_json["test_name"] = "ndt";
-    final_json["test_runtime"] = results["timesec"];
-    final_json["test_start_time"] = start_date;
-    final_json["test_version"] = "1.0";
-    return final_json;
-}
 
-function onstart_cb(server, onstart_call) {
-    this.server = server;
-    this.now = new Date().toISOString().replace('T', ' ').substr(0, 19);
-    if (onstart_call)
-        onstart_call(server)
-};
+    function onstart_cb(server, onstart_call) {
+        this.server = server;
+        this.now = new Date().toISOString().replace('T', ' ').substr(0, 19);
+        if (onstart_call)
+            onstart_call(server)
+    };
 
-function onfinish_cb(results, onfinish_call) {
-    var final_json = json_result_formatter(results, this.now, this.server)
-    console.log(JSON.stringify(final_json));
-    if (onfinish_call)
-        onfinish_call(results);
-    return;
-};
+    function onfinish_cb(results, onfinish_call) {
+        var final_json = json_result_formatter(results, this.now, this.server)
+        if (onfinish_call) 
+            onfinish_call(final_json);
+        else
+            console.log(JSON.stringify(final_json));
+        return
+    };
 
-function onerror_cb(error_message, onerror_call) {
-    console.log(error_message);
-    if (onerror_call)
-        onerror_call(error_message);
-}
+    function onerror_cb(error_message, onerror_call) {
+        console.log(error_message);
+        if (onerror_call)
+            onerror_call(error_message);
+    };
 
-function generate_callback(overwriting_callbacks){
-var callbacks = {
-    'onstart': function(server) {
-        var onstart_call;
-        if (overwriting_callbacks && overwriting_callbacks["onstart"])
-            onstart_call = overwriting_callbacks["onstart"]
-        onstart_cb(server, onstart_call);
-    },
-    'onstatechange': function(state, results) {
-        if (overwriting_callbacks && overwriting_callbacks["onstatechange"])
-            overwriting_callbacks["onstatechange"](state, results)
-    },
-    'onfinish': function(passed_results) {
-        var onfinish_call;
-        if (overwriting_callbacks && overwriting_callbacks["onfinish"])
-            onfinish_call = overwriting_callbacks["onfinish"]
-        onfinish_cb(passed_results, onfinish_call);
-    },
-    'onprogress': function(state, results) {
-        if (overwriting_callbacks && overwriting_callbacks["onprogress"])
-        overwriting_callbacks["onprogress"](state, results)
-    },
-    'onerror': function(error_message) {
-        var onerror_call;
-        if (overwriting_callbacks && overwriting_callbacks["onerror"])
-            onerror_call = overwriting_callbacks["onerror"]
-        onerror_cb(error_message, onerror_call);
-    }
-  };
-  return callbacks;
-}
-
-/*function find_ndt_server(callback) {
-    request.get('http://mlab-ns.appspot.com/ndt?format=json', (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            var importedJSON = JSON.parse(body);
-            var mlab_server_metro = importedJSON.url;
-            var parsed_url = url.parse(mlab_server_metro, true);
-            var clientNDTWrapper = new NDTWrapper(parsed_url.hostname, 3001, generate_callback())
-            clientNDTWrapper.run_test()
-        }
-    });
-}*/
-
-
-/*function find_ndt_server(callback) {
-    getJSON('http://mlab-ns.appspot.com/ndt?format=json', function (err, response) {
-        if (err) {
-            console.log("error retrieving mlab server list: "+err);
-            process.exit(144);
-        }
-        var parsed_url_hostname = url.parse(response.url, true).hostname;
-        var clientNDTWrapper = new NDTWrapper(parsed_url_hostname, 3001, generate_callback());
-        clientNDTWrapper.run_test();
-        return;
-    });
-}*/
-
-function find_ndt_server(callback) {
-        var request = http.get("http://mlab-ns.appspot.com/ndt?format=json", function(response){ 
-        var body = ""; 
-        response.on("data", function(chunk){ 
-            body += chunk; 
-        });
-        response.on("end", function(){ 
-            if(response.statusCode === 200){ 
-                try{ var profile = JSON.parse(body); 
-                    var mlab_server_metro = JSON.parse(body).url;
-                    var parsed_url_hostnamename = url.parse(mlab_server_metro, true).hostname;
-                    var clientNDTWrapper = new NDTWrapper(parsed_url_hostnamename, 3001, generate_callback())
-                    clientNDTWrapper.run_test()
-                } catch(error){ 
-                    console.error(error.message); 
-                }
-            } else {
-                console.error("There was an error getting mlab server");
+    function generate_callback(overwriting_callbacks) {
+        var callbacks = {
+            'onstart': function (server) {
+                var onstart_call;
+                if (overwriting_callbacks && overwriting_callbacks["onstart"])
+                    onstart_call = overwriting_callbacks["onstart"]
+                onstart_cb(server, onstart_call);
+            },
+            'onstatechange': function (state, results) {
+                if (overwriting_callbacks && overwriting_callbacks["onstatechange"])
+                    overwriting_callbacks["onstatechange"](state, results)
+            },
+            'onfinish': function (passed_results) {
+                var onfinish_call;
+                if (overwriting_callbacks && overwriting_callbacks["onfinish"])
+                    onfinish_call = overwriting_callbacks["onfinish"]
+                onfinish_cb(passed_results, onfinish_call);
+            },
+            'onprogress': function (state, results) {
+                if (overwriting_callbacks && overwriting_callbacks["onprogress"])
+                    overwriting_callbacks["onprogress"](state, results)
+            },
+            'onerror': function (error_message) {
+                var onerror_call;
+                if (overwriting_callbacks && overwriting_callbacks["onerror"])
+                    onerror_call = overwriting_callbacks["onerror"]
+                onerror_cb(error_message, onerror_call);
             }
-        });
-    });
-}
+        };
+        return callbacks;
+    };
 
-find_ndt_server()
+    function find_ndt_server(callback) {
+        var request = http.get("http://mlab-ns.appspot.com/ndt?format=json", function (response) {
+            var body = "";
+            response.on("data", function (chunk) {
+                body += chunk;
+            });
+            response.on("end", function () {
+                if (response.statusCode === 200) {
+                    try {
+                        var profile = JSON.parse(body);
+                        var mlab_server_metro = JSON.parse(body).url;
+                        var parsed_url_hostnamename = url.parse(mlab_server_metro, true).hostname;
+                        var clientNDTWrapper = new NDTWrapper(parsed_url_hostnamename, 3001, generate_callback(callback))
+                        clientNDTWrapper.run_test()
+                    } catch (error) {
+                        console.error(error.message);
+                    }
+                } else {
+                    console.error("There was an error getting mlab server");
+                }
+            });
+        });
+    };
+    this.run_test = function (callback) {
+        find_ndt_server(callback);
+        return;
+    };
+};
+module.exports = new NodeNdt();
